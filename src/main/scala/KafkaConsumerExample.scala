5d085b9b0e5e21ab874d30f9a00f489630c90545
RajeshS <rajesh.santha29@gmail.com>
2019-05-15 21:54:36 +0530

    added more consumer and producer examples

--- src/main/scala/KafkaConsumerExample.scala
+++ src/main/scala/KafkaConsumerExample.scala
@@ -1,25 +1,32 @@
 import java.util.{Collections, Properties}
-
 import com.typesafe.config.ConfigFactory
 import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
-
 import scala.collection.JavaConversions._
+import org.apache.kafka.common.TopicPartition
+import java.util
 
 object KafkaConsumerExample {
 
-  def main(args: Array[String]): Unit = {
+  def main (args: Array[String]): Unit = {
     val conf = ConfigFactory.load(this.getClass().getClassLoader())
     val envProps = conf.getConfig(args(0))
+    //    val envProps = conf.getConfig("prod")
+
     println(envProps)
     println(envProps.getString("topic"))
     val props = new Properties()
     props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, envProps.getString("bootstrap.server"))
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
-    props.put(ConsumerConfig.GROUP_ID_CONFIG, "1")
-
+    //props.put(ConsumerConfig.GROUP_ID_CONFIG, "1")
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
     val consumer = new KafkaConsumer[String, String](props)
-    consumer.subscribe(Collections.singletonList(envProps.getString("topic")))
+   // consumer.subscribe(Collections.singletonList(envProps.getString("topic")))
+    // consumer.subscribe(Collections.singletonList("rajeshs_t1"))
+    val tp = new TopicPartition("rajeshs_t1", 0)
+    val tps = util.Arrays.asList(tp)
+    consumer.assign(tps)
+    consumer.seekToBeginning(tps)
     while (true) {
       val records = consumer.poll(500)
       for (record <- records.iterator()) {
--- src/main/scala/KafkaProducerExample.scala
+++ src/main/scala/KafkaProducerExample.scala
@@ -8,15 +8,20 @@ object KafkaProducerExample {
   def main (args: Array[String]): Unit = {
     val conf = ConfigFactory.load(this.getClass().getClassLoader())
     val envConfig = conf.getConfig(args(0))
+//    val envConfig = conf.getConfig("prod")
+
     println(envConfig)
     val props = new Properties()
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, envConfig.getString("bootstrap.server"))
     props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducerExample")
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+
     val producer = new KafkaProducer[String, String](props)
     println("producer created")
-    val data = new ProducerRecord[String, String](envConfig.getString("topic"), "hello", "Universe")
+   val data = new ProducerRecord[String, String](envConfig.getString("topic"), 0,"hello", "Universe")
+   // val data = new ProducerRecord[String, String]("rajeshs_t1", "hello", "Universe")
+
     println(data.topic() + "\n" + data.partition() + "\n" + data.toString)
     producer.send(data)
     println("sent msg")
