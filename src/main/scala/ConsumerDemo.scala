a88ec41b27ae514a40f86bf1d30c34bd36890f8e
Rajesh Santha <rajesh.santha29@gmail.com>
2020-09-18 15:51:47 +0530

    update ConsumerDemo

--- src/main/scala/ConsumerDemo.scala
+++ src/main/scala/ConsumerDemo.scala
@@ -1,54 +1,45 @@
+package com.example
+
+
+import java.text.SimpleDateFormat
+import java.util.{Collections, Properties}
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+
+import scala.collection.JavaConversions._
+import org.apache.kafka.common.TopicPartition
 import java.util
-import java.util.Properties
-import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.common.serialization.StringDeserializer
-import scala.collection.JavaConverters._
-
-
 
 object ConsumerDemo {
+  val dateFormat = "yyyy-MM-dd HH:mm:ss"
+  val epochToDate: Long => String = new SimpleDateFormat(dateFormat).format(_)
 
+  def main(args: Array[String]): Unit = {
 
-  def main (args: Array[String]): Unit = {
-
-    /**
-      * export KAFKA_HOME=/usr/hdp/current/kafka-broker
-      * export PATH=$PATH:$KAFKA_HOME/bin
-      * export zookeepers=nn01.itversity.com:2181,nn02.itversity.com:2181,rm01.itversity.com:2181
-      * export brokerlist=wn01.itversity.com:6667,wn02.itversity.com:6667,wn03.itversity.com:6667
-      * export bootstrapservers=wn01.itversity.com:6667,wn02.itversity.com:6667,wn03.itversity.com:6667
-      */
-    //val zookeepers = "nn01.itversity.com:2181,nn02.itversity.com:2181,rm01.itversity.com:2181"
-    val bootstrapServers = "wn01.itversity.com:6667,wn02.itversity.com:6667,wn03.itversity.com:6667"
-    //val brokerlist = "wn01.itversity.com:6667,wn02.itversity.com:6667,wn03.itversity.com:6667"
-    val groupId = "my-third-application"
-    val topic = "rajeshs_t1"
-
-    import org.apache.kafka.clients.consumer.ConsumerConfig
-    val properties = new Properties()
-    properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
-    properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getCanonicalName)
-    properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getCanonicalName)
-    properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId)
-    properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-    println(properties.toString)
-    println("properties done")
-    val consumer = new KafkaConsumer[String, String](properties)
-//    consumer.subscribe(util.Collections.singletonList(topic))
-    // subscribe consumer to our topic(s)
-    consumer.subscribe(util.Arrays.asList(topic))
-    println("")
-    // poll for new data
-    println("poll for new data")
+    val props = new Properties()
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "consumergroup1_rajesh")
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+    val consumer = new KafkaConsumer[String, String](props)
+    // consumer.subscribe(Collections.singletonList(envProps.getString("topic")))
+    // consumer.subscribe(Collections.singletonList("rajeshs_t1"))
+    val tp = new TopicPartition("Testtopic1", 0)
+    val tps = util.Arrays.asList(tp)
+    consumer.assign(tps)
+    consumer.seekToBeginning(tps)
     while (true) {
-      println("before record")
-      val record = consumer.poll(10L).asScala
-      println("record.isEmpty = "+record.isEmpty)
-      for (data <- record.iterator)
-        println(data.value())
+      val records = consumer.poll(500)
+      for (record <- records.iterator()) {
+        println("received metadata : \n " +
+          "Topic      : " + record.topic() + "\n" +
+          "Partiton   : " + record.partition() + "\n" +
+          "Offset     : " + record.offset() + "\n" +
+          "TimeStamp  : " + record.timestamp() + "\n" +
+          "Date  : " + epochToDate(record.timestamp()) + "\n" +
+          "Value      : " + record.value())
+      }
     }
-
   }
-
-
 }
